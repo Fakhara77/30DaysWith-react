545f05e90f6a7bf0212a109f37dc31bb66f97de4
Fakhara77 <fakharatasneem77@gmail.com>
2023-08-28 07:56:21 -0700

    declear the other ways

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Q1:Declare an empty array;
 let arr=Array(); 
 console.log(arr);
+//second way to declear empty the array 
+let array=[];
+console.log(array);
+//3rd way to declear  empty array
+let arr1=([]);
+console.log(arr1);
 
 //Declare an array with more than 5 number of elements;
-const array=['e1', 'e2', 'e3', 'e4', 'e5', 'e6'];
-console.log(array);
+const array2=['e1', 'e2', 'e3', 'e4', 'e5', 'e6'];
+console.log(array2);
