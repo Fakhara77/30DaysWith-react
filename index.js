048e68065452da5f1b1f291c69ff5f93eae8279b
Fakhara77 <fakharatasneem77@gmail.com>
2023-08-28 03:44:00 -0700

    solved Question2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Q1:Declare an empty array;
 let arr=Array(); 
 console.log(arr);
+
+//Declare an array with more than 5 number of elements;
+const array=['e1', 'e2', 'e3', 'e4', 'e5', 'e6'];
+console.log(array);
