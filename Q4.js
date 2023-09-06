2afdda13b947723b077f1196b1a05007fd02448b
Fakhara77 <fakharatasneem77@gmail.com>
2023-09-06 08:26:20 -0700

    Object Exercise solved

---
+++ Q4.js
@@ -0,0 +1,11 @@
+//Get name, legs, color, age and bark value from the dog object
+const dog={
+    name:'puppy',
+    legs:'four',
+    color:'brown && white',
+    age:7,
+    bark:function() {
+       return 'woof ,woof' ;
+    }
+}
+console.log(dog);
---
+++ Q5.js
@@ -0,0 +1,10 @@
+//Set new properties the dog object: breed, getDogInfo
+const Dog={
+   name:'puppy',
+   weight:'20kg',
+   age:'2 year',
+   getDoginfo:function(){
+    return 'breed';
+   }
+}
+console.log(Dog.getDoginfo());
